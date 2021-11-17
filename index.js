710cf05d2678a25eaa7235fc6ce961546abbe2f7
xufeng9588 <91304132+xufeng9588@users.noreply.github.com>
2021-11-17 12:08:55 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const config = [{
-    url:'/linear-swap-ex/market/history/kline/',
-    baseURL:"https://api.hbdm.com",
-    contract_code:'contract_code=BTC-USDT',
-    period:'period=1day',
-    from:'from=1587052800',
-    to:'to=1591286400'
-}]
-module.exports={
-    config
+const fs = require('fs');
+const { getresult } = require('./utils/huobi');
+
+function getData(){
+    setInterval(() => {
+        const result = getHuoBiResult();
+        // console.log(result)
+        const t = result[0];
+        console.log(t)
+    }, 1*60*1000);
 }
+getData()
